314c96ef86c1e060bd3a114d94aeabfce64e4b5b
sadanandwanole <118704192+sadanandwanole@users.noreply.github.com>
2023-01-26 13:10:55 +0530

    Add files via upload

---
+++ springrest/src/main/java/com/jspiders/springrest/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.jspiders.springrest.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+
+}
---
+++ springrest/src/main/java/com/jspiders/springrest/service/StudentService.java
@@ -0,0 +1,8 @@
+package com.jspiders.springrest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+}
